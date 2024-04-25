f5fedb64ce769df0cdc3480c2c7b4f1f5e3c52f5
Bukunmiemma <bogunsola38@gmail.com>
2024-04-25 01:30:54 +0100

    step-2

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:my_notes/firebase_options.dart';
 import 'package:my_notes/views/login_view.dart';
-import 'firebase_options.dart';
+import 'package:my_notes/views/register_view.dart';
+
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,44 +15,21 @@ void main() {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const RegisterView(),
+      home: const HomePage(),
+      routes: {
+        '/login/': (context) => const LoginView(),
+        '/register/' :(context) => const RegisterView(),
+      },
     )
     );
 }
 
-class RegisterView extends StatefulWidget {
-  const RegisterView({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
-  @override
-  State<RegisterView> createState() => _RegisterViewState();
-}
-
-class _RegisterViewState extends State<RegisterView> {
-  late final TextEditingController _email;
-  late final TextEditingController _password;
-
-  @override
-  void initState() {
-    _email = TextEditingController();
-    _password = TextEditingController();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-   _email.dispose();
-   _password.dispose();
-    super.dispose();
-  }
-
-  @override
+   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Register'
-        ),
-      ),
-      body: FutureBuilder(
+    return FutureBuilder(
         future: Firebase.initializeApp(
         options: DefaultFirebaseOptions.currentPlatform,
         ),
@@ -57,62 +37,20 @@ class _RegisterViewState extends State<RegisterView> {
           switch (snapshot.connectionState){
  
             case ConnectionState.done:
-            return Column(
-          children: [
-            TextField(
-              controller: _email,
-              enableSuggestions: false,
-              autocorrect: false,
-              keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(
-                hintText: 'Enter your email here'
-              ),
-            ),
-            TextField(
-              controller: _password,
-              obscureText: true,
-              enableSuggestions: false,
-              autocorrect: false,
-              decoration: const InputDecoration(
-                hintText: 'Enter your password here'
-              ),
-            ),
-            TextButton(   
-              onPressed: ()  async{
-              
-              try{
-                final email = _email.text;
-              final password = _password.text;
-              final userCredential =
-              await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                email: email,
-                 password: password,
-                 );
-                 print('userCredential');
-              } on FirebaseAuthException catch(e) {
-              if (e.code == 'weak-password') {
-                print('Weak password');
-              } else if (e.code == 'email-already-in-use') {
-                  print('Email is already in use');
-              } else if (e.code == 'invalid-email') {
-                  print('Invalid emmail entered');
-              }
-               
-              }
-              
-               },
-               child: const Text('Register'),
-            ),
-          ],
-        );  
+            // final user = FirebaseAuth.instance.currentUser;
+            // if (user?.emailVerified ?? false) {
+            //   return const Text('Done');
+            // } else{
+            //    return const VerifyEmailView(); 
+            // }
+               return const LoginView();
               default:
-              return const Text('Loading...');
+              return const CircularProgressIndicator();
           } 
         
         },
          
-      ),
-    );
+      );
   }
 }
 

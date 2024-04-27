6c675591c71f5e516fb43ca6f138f52440760ab4
Bukunmiemma <bogunsola38@gmail.com>
2024-04-27 17:13:41 +0100

    step-4

--- lib/main.dart
+++ lib/main.dart
@@ -20,8 +20,9 @@ void main () {
       ),
       home: const HomePage(),
       routes: {
-        '/login/': (context) => const LoginView(),
-        '/register/' :(context) => const RegisterView(),
+        '/login/' : (context) => const LoginView(),
+        '/register/' : (context) => const RegisterView(),
+        '/notes/' : (context) =>const NotesView(),
       },
     )
     );
